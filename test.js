b20e95d055552023b7833ac382fb17a248076a03
Amosnimos <76786560+AmosNimos@users.noreply.github.com>
2026-04-02 20:13:56 -0400

    Update and rename external.js to test.js

--- test.js
+++ test.js
@@ -3,10 +3,6 @@ export const filesystem = {
   home: {
     user: {
       "readme.txt": "Hello World!"
-    },
-    documents: {},          // empty folder
-    projects: {
-      "project1.txt": "Project 1 details"
     }
   }
 };
