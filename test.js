3931b0339f9840e95bb0aaea76daabb298863e1e
Amosnimos <76786560+AmosNimos@users.noreply.github.com>
2026-04-02 20:09:09 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 // external.js
 export const filesystem = {
-  home: { user: { "readme.txt": "Hello World!" } }
+  home: {
+    user: {
+      "readme.txt": "Hello World!"
+    },
+    documents: {},          // empty folder
+    projects: {
+      "project1.txt": "Project 1 details"
+    }
+  }
 };
